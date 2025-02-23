ec6a150a9a5a8d743935bddf60c2f0d73c8dc01b
jialong0518 <jialong0518@qq.com>
2025-02-23 11:29:34 +0800

    Initial commit

---
+++ src/utils/get-page-title.js
@@ -0,0 +1,10 @@
+import defaultSettings from '@/settings'
+
+const title = defaultSettings.title || 'AI艺术图像生成'
+
+export default function getPageTitle (pageTitle) {
+  if (pageTitle) {
+    return `${pageTitle} - ${title}`
+  }
+  return `${title}`
+}
